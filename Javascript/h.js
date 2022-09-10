288b1daed2895b5ede4f062cb35e20b105ae9115
Tarun Guntaka <34502504+tarunguntaka@users.noreply.github.com>
2022-09-10 19:45:09 -0400

    update

---
+++ Javascript/h.js
@@ -0,0 +1,49 @@
+// console.log -> alows to see things in console 
+
+
+// comment 
+
+var t = 'yo homies';
+
+// var - whole program 
+// let - within the scope 
+// const - constant that can never change 
+
+var a;
+var b = 2;
+
+console.log(a,b)
+console.log("Hello\n from Tarun \n \t")
+
+
+l  = t.length;
+
+console.log(t[0])
+
+
+
+function wb(name,id){
+
+    var name = name
+    var id = id 
+    console.log(name, id)
+}
+
+console.log("Puka",1);
+
+
+var arr1 = [1,2,3,4,"Puka"];
+
+console.log(arr1[(arr1.length)-1]);
+
+// append  -> push()
+
+arr1.push("lanja","munda")
+
+console.log(arr1)
+// pop()
+
+
+//shift() remove the first element 
+// unshift() - adds an element to the begining of the array 
+
