3ee25f171beb696d67137d64d348e1ba3160ed84
Tarun Guntaka <tarunguntaka@gmail.com>
2022-09-11 01:54:13 -0400

    updating

--- Javascript/h.js
+++ Javascript/h.js
@@ -44,6 +44,81 @@ console.log(arr1)
 // pop()
 
 
-//shift() remove the first element 
+//shift() removed the first element 
 // unshift() - adds an element to the begining of the array 
+
+
+// global scope 
+
+var a1 = 10;
+var b1;
+
+function add1(a,b){
+    if(typeof a !="undefined"){
+        console.log("puvva");
+    }
+    if(typeof b !="undefined"){
+        console.log("puvva");
+    }
+    //console.log(a+b);
+
+}
+
+
+add1(a1,b1)
+// if you dont put  the var keyword , the variable becomes global 
+
+// you can have same global and local var as same name , local var has the first preference 
+
+// booleans true or false 
+
+// == and === check it  == does a type conversion 
+// && - and , || - or operators 
+
+// switch case uses strict equality operator === 
+
+
+
+
+
+
+// creating objects 
+
+var ourdog = {
+    "name":"boom",
+    "age":10,
+    "friends": 0 
+}
+
+console.log(ourdog.age)
+
+ourdog.name = "lanja"
+
+console.log(ourdog.name)
+
+ourdog.married = "Nope"
+
+console.log(ourdog.married)
+
+
+delete ourdog.age
+
+
+// to check if a property exists or not we use obj.hasOwnProperty(property_name_to check) function 
+
+
+
+// parseInt(string) func
+
+// mainly use const and let , let is block scope 
+
+// Object.freeze(object_name)  --- to freeze an object 
+
+// map filter and reduce 
+
+// arrow functions 
+// rest operator 
+// spreaD operator 
+
+// structuring and destructuring assignments 
 
